cbb48a3222158d1e1191decc9651aa94cae82fbc
Daniel Champagne <danchampagne@hotmail.com>
2019-11-01 10:48:31 +0000

    Form and Submit

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,4 +2,20 @@ class BeersController < ApplicationController
   def index
     @beers = Beer.all
   end
+
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    Beer.create(beer_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def beer_params
+    params.require(:beer).permit(:name, :brewery, :description)
+  end
+
 end
